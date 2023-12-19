66ea71962e998c02ea0710bb788dbdbf2892e5c2
Metolina <mertyurekir@gmail.com>
2023-12-19 23:13:59 +0300

    Add project files.

---
+++ Shared/FreeCourse.Shared/Dtos/ResponseDto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace FreeCourse.Shared.Dtos
+{
+	public class Response<T>
+	{
+		public T Data { get; private set; }
+		[JsonIgnore]
+		public int StatusCode { get; private set; }
+		[JsonIgnore]
+		public bool IsSuccessful { get; set; }
+		public List<string> Errors { get; set; }
+
+
+		//static factory method
+		public static Response<T> Success(T data, int statusCode)
+		{
+			return new Response<T> { Data = data, StatusCode = statusCode };
+		}
+
+		public static Response<T> Success(int statusCode)
+		{
+			return new Response<T> { Data = default(T), StatusCode = statusCode };
+
+		}
+
+		public static Response<T> Fail(List<string> errors, int statusCode)
+		{
+			return new Response<T>
+			{
+				Errors = errors,
+				StatusCode = statusCode,
+				IsSuccessful = false
+			};
+		}
+		public static Response<T> Fail(string Error, int statusCode)
+		{
+			return new Response<T>
+			{
+				Errors = new List<string>() { Error },
+				StatusCode = statusCode,
+				IsSuccessful = false
+			};
+		}
+
+
+
+	}
+}
