30dddb9e9a3013867410b0c0b4e2ed6aa1e8e946
Metolina <mertyurekir@gmail.com>
2023-12-21 00:00:27 +0300

    <PhotoStock> Projesi eklenildi ve korumaya alındı. -Testleri kaldı. <PhotoStock> projesi içerisinde Upload ve Delete gibi işlemler için Endpointler hazırlandı.

---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,44 @@
+﻿using FreeCourse.Services.PhotoStock.Dtos;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.PhotoStock.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PhotosController : CustomBaseController
+	{
+		[HttpPost]
+		public async Task<IActionResult> PhotoSave(IFormFile photo,CancellationToken cancellationToken)
+		{
+			if (photo != null && photo.Length!=0)
+			{
+				var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+				using var stream = new FileStream(path, FileMode.Create);
+				await photo.CopyToAsync(stream, cancellationToken);
+				var returnPath="photos/"+photo.FileName;
+				PhotoDto photoDto = new() { Url = returnPath };
+				return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto,200));
+			}
+			return CreateActionResultInstance(Response<PhotoDto>.Fail("Fotoğraf Dosyası Bulunamadı", 400));
+			
+		}
+		[HttpDelete]
+		public IActionResult PhotoDelete(string photoUrl)
+		{
+			var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+			if (!System.IO.File.Exists(path))
+			{
+				return CreateActionResultInstance(Response<NoContent>.Fail("Fotoğraf Bulunamadı", 404));
+			}
+			System.IO.File.Delete(path);
+			return CreateActionResultInstance(Response<NoContent>.Success(204));
+
+		}
+	}
+}
