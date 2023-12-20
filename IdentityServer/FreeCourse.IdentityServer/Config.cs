6c234d7cc3a3e69f8f7c1adc47602f6d79094e30
Metolina <mertyurekir@gmail.com>
2023-12-20 12:23:49 +0300

    İdentity Server Config ve startup düzenlemeleri

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -2,29 +2,47 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityModel;
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
 namespace FreeCourse.IdentityServer
 {
-    public static class Config
-    {
-        public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
-             
-                   };
+	public static class Config
+	{
 
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
-            {
-              
-            };
+		public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+		{
+			new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
+			new ApiResource("photo_stock_catalog"){Scopes={"photo_stock_fullpermission"}},
+			new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
+		};
+		public static IEnumerable<IdentityResource> IdentityResources =>
+				   new IdentityResource[]
+				   {
 
-        public static IEnumerable<Client> Clients =>
-            new Client[]
-            {
-              
-            };
-    }
+				   };
+
+		public static IEnumerable<ApiScope> ApiScopes =>
+			new ApiScope[]
+			{
+				new ApiScope("catalog_fullpermission","Catalog API için Full Erişim"),
+				new ApiScope("photo_stock_fullpermission","PhotoStok API için Full Erişim"),
+				new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
+			};
+
+		public static IEnumerable<Client> Clients =>
+			new Client[]
+			{
+				new Client
+				{
+					ClientName="Asp.Net Core MVC",
+					ClientId="WebMvcClient",
+					ClientSecrets={new Secret("secret".ToSha256())},
+					AllowedGrantTypes=GrantTypes.ClientCredentials,
+					AllowedScopes={"catalog_fullpermission", "photo_stock_fullpermission",IdentityServerConstants.LocalApi.ScopeName}
+				}
+			};
+	}
 }
