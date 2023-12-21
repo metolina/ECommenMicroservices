8a38542c5199a5fca08d4c8ad0fb77c215322aa1
MertYürekir <mert.yurekir@ydyazilim.com>
2023-12-21 16:51:51 +0300

    <IdentityService> -<BasketService> eklenilmesi tamamlandı
    <BasketService>
    -Controller added
    -İmplant added
    -Interface added
    
    <Shared>
    -Redis settings added

---
+++ Services/Basket/FreeCourse.Sservices.Basket/Services/IBasketService.cs
@@ -0,0 +1,13 @@
+﻿using FreeCourse.Shared.Dtos;
+using FreeCourse.Sservices.Basket.Dtos;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Sservices.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<Response<BasketDto>> GetBasket(string userId);
+        Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+        Task<Response<bool>> Delete(string userId);
+    }
+}
---
+++ Services/Basket/FreeCourse.Sservices.Basket/Settings/RedisSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace FreeCourse.Sservices.Basket.Settings
+{
+    public class RedisSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
